7715143c9d5192bdadaade45638bbb45591bb7f0
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-04-07 20:17:10 +0000

    Add exit prevent code to be executed when  token is missing / wrong

--- func/cloudflare.sh
+++ func/cloudflare.sh
@@ -1,11 +1,21 @@
 get_api_token(){
-    source $HESTIA/data/users/$user/cloudflare.conf
-    success=$(curl -s -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
-     -H "Authorization: Bearer $TOKEN" | jq -r '.success');
-     
-    if [ $success != 'true' ]; then
-    echo "Unable to connect to Cloudflare API"
-    exit 3;
+    if [ -e "$HESTIA/data/users/$user/cloudflare.conf" ]; then 
+        source $HESTIA/data/users/$user/cloudflare.conf
+        if [ ! -z "$TOKEN" ]; then
+            success=$(curl -s -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
+             -H "Authorization: Bearer $TOKEN" | jq -r '.success');
+             
+            if [ $success != 'true' ]; then
+                echo "Unable to connect to Cloudflare API"
+                exit 3;
+            fi
+        else
+            echo "Unable to connect to Cloudflare API"
+            exit 3;
+        fi
+    else
+        echo "No Cloudflare token set"
+        exit 3;
     fi
 }
 
