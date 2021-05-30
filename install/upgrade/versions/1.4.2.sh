9f787fdbba91a2371289abe888b19fe5688bec9f
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-05-31 00:11:52 +0200

    Add extra command to stop de firewall
    To force generate the new rules

--- install/upgrade/versions/1.4.2.sh
+++ install/upgrade/versions/1.4.2.sh
@@ -9,6 +9,8 @@
 # Optimize loading firewall rules
 if [ "$FIREWALL_SYSTEM" = "iptables" ]; then
     echo "[ * ] Fix the issue of loading firewall rules..."
+    # stop firewall then delete the rules so all rules are missing then regenerate them
+    $BIN/v-stop-firewall
     rm -f /etc/iptables.rules
     rm -f /usr/lib/networkd-dispatcher/routable.d/50-ifup-hooks /etc/network/if-pre-up.d/iptables
     $BIN/v-update-firewall
