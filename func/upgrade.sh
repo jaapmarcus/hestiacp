9f0ae521e93dac614039eeffade710a658598742
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-05-11 14:28:49 +0200

    Update correct file

--- func/upgrade.sh
+++ func/upgrade.sh
@@ -571,7 +571,7 @@ upgrade_b2_tool(){
 upgrade_cloudflare_ip() {
     echo "[ * ] Update Cloudflare IP"
     # https://github.com/ergin/nginx-cloudflare-real-ip/
-    CLOUDFLARE_FILE_PATH='/etc/nginx/cloudflare.conf'
+    CLOUDFLARE_FILE_PATH='/etc/nginx/conf.d/cloudflare.conf'
     echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH;
     echo "" >> $CLOUDFLARE_FILE_PATH;
     
