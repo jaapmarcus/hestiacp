e8764505647f1a4233dbeb244f075bd856648424
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-05-11 14:46:25 +0200

    Resolve issue in sed code

--- install/upgrade/versions/1.6.0.sh
+++ install/upgrade/versions/1.6.0.sh
@@ -76,7 +76,7 @@ fi
 # Update Cloudflare address
 if [ -f /etc/nginx/nginx.conf ] && [ ! -f /etc/nginx/conf.d/cloudflare.conf ]; then
     sed -i '/set_real_ip_from/d' /etc/nginx/nginx.conf 
-    sed -i 'real_ip_header CF-Connecting-IP;/d' /etc/nginx/nginx.conf 
-    sed -i 's/# Cloudflare https:\/\/www.cloudflare.com\/ips/# Cloudflare https:\/\/www.cloudflare.com\/ips\n    include \/etc\/nginx\/conf.d/cloudflare.conf/' /etc/nginx/nginx.conf 
+    sed -i '/real_ip_header CF-Connecting-IP;/d' /etc/nginx/nginx.conf 
+    sed -i 's|# Cloudflare https://www.cloudflare.com/ips|# Cloudflare https://www.cloudflare.com/ips\n    include /etc/nginx/conf.d/cloudflare.conf|g' /etc/nginx/nginx.conf 
     # At a later stage a function  will run and will load all the new rules
 fi
